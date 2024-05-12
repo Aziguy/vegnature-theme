163637ffc55e3f828a491c30f300b3637ad7ed3f
Aziguy <monsieur_hernandez@hotmail.com>
2024-05-12 09:09:57 +0200

    Fix: Resolve an issue into php contact form code.

--- inc/contact/contact-process.php
+++ inc/contact/contact-process.php
@@ -1,51 +1,41 @@
 <?php
-$address = "vegnature@vegnature.fr";
+
+$address = "mandatairessm@gmail.com";
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
 $error = false;
-$fields = array( 'name', 'email', 'message' );
+$fields = array('name', 'email', 'subject', 'message');
 
-foreach ( $fields as $field ) {
-	if ( empty($_POST[$field]) || trim($_POST[$field]) == '' )
-		$error = true;
+foreach ($fields as $field) {
+    if (empty($_POST[$field]) || trim($_POST[$field]) == '') {
+        $error = true;
+    }
 }
 
-if ( !$error ) {
-	$name = stripslashes($_POST['name']);
-	$email = trim($_POST['email']);	
-	$subject = stripslashes($_POST['subject']);
-	$message = stripslashes($_POST['message']);
-	$e_subject = 'Vous avez été contacté par ' . $name . '.';
-	
+if (!$error) {
+    $name = stripslashes($_POST['name']);
+    $email = trim($_POST['email']);
+    $subject = stripslashes($_POST['subject']);
+    $message = stripslashes($_POST['message']);
+    $e_subject = 'Vous avez été contacté par ' . $name;
 
-	// Configuration option.
-	// You can change this if you feel that you need to.
-	// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
+    $e_body = "Vous avez été contacté par : $name" . PHP_EOL . PHP_EOL;
+    $e_reply = "E-mail: $email" . PHP_EOL . PHP_EOL;
+    $e_content = "Message:" . PHP_EOL . $message . PHP_EOL . PHP_EOL;
+    $e_content .= "Noms: $name" . PHP_EOL;
+    $e_content .= "Objet: $subject" . PHP_EOL;
 
-	$e_body = "Vous avez été contacté par : $name" . PHP_EOL . PHP_EOL;
-	$e_reply = "E-mail: $email" . PHP_EOL . PHP_EOL;
-	$e_content = "Message:\r\n$message \r\n Noms: $name \r\n Objet: $e_subject" . PHP_EOL;
-	
+    $msg = wordwrap($e_body . $e_reply, 70);
 
-	$msg = wordwrap( $e_body . $e_reply , 70 );
+    $headers = "From: $email" . PHP_EOL;
+    $headers .= "Reply-To: $email" . PHP_EOL;
+    $headers .= "MIME-Version: 1.0" . PHP_EOL;
+    $headers .= "Content-Type: text/plain; charset=UTF-8" . PHP_EOL;
 
-	$headers = "From: $email" . PHP_EOL;
-	$headers .= "Reply-To: $email" . PHP_EOL;
-	$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-	$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
-
-	if (mail($address, $msg, $headers, $e_content  )) {
-
-		// Email has sent successfully, echo a success page.
-	
-		echo 'Success';
-
-	} else {
-
-		echo 'ERROR!';
-
-	}
-
+    if (mail($address, $e_subject, $e_content, $headers)) {
+        echo 'Success';
+    } else {
+        echo 'ERROR!';
+    }
 }
-
 ?>
